5fac7fdfc267caf30e38c8ade0978fcfd14e8c4f
alex v <alex@alohechotecho.com>
2017-02-22 14:02:01 +0100

    Add VERSIONBITS_LAST_OLD_BLOCK_VERSION as new blocks version

--- src/miner.cpp
+++ src/miner.cpp
@@ -199,7 +199,7 @@ CBlockTemplate* BlockAssembler::CreateNewBlock(const CScript& scriptPubKeyIn, bo
         UpdateTime(pblock, chainparams.GetConsensus(), pindexPrev);
     pblock->nBits          = fProofOfStake ? GetNextTargetRequired(pindexPrev, true) : GetNextWorkRequired(pindexPrev, pblock, chainparams.GetConsensus());
     pblock->nNonce         = 0;
-    pblock->nVersion       = 0x7;
+    pblock->nVersion       = VERSIONBITS_LAST_OLD_BLOCK_VERSION;
     pblocktemplate->vTxSigOpsCost[0] = WITNESS_SCALE_FACTOR * GetLegacySigOpCount(pblock->vtx[0]);
 
     if (pFees)
