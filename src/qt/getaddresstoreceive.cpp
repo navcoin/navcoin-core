ea198710e4a35695751bb5397921e2ac55643178
alex v <alex@alohechotecho.com>
2017-03-07 01:11:19 +0100

    getaddresstoreceive

--- src/qt/getaddresstoreceive.cpp
+++ src/qt/getaddresstoreceive.cpp
@@ -1,14 +1,93 @@
+#include "guiconstants.h"
+#include "guiutil.h"
+#include "walletmodel.h"
+#include "addresstablemodel.h"
+
 #include "getaddresstoreceive.h"
 #include "ui_getaddresstoreceive.h"
+
+#ifdef USE_QRCODE
+#include <qrencode.h>
+#endif
 
 getAddressToReceive::getAddressToReceive(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::getAddressToReceive)
 {
     ui->setupUi(this);
+
+    connect(ui->requestPaymentButton,SIGNAL(clicked()),this,SLOT(showRequestPayment()));
+    connect(ui->copyClipboardButton,SIGNAL(clicked()),this,SLOT(copyToClipboard()));
+    connect(ui->newAddressButton,SIGNAL(clicked()),this,SLOT(getNewAddress()));
 }
 
 getAddressToReceive::~getAddressToReceive()
 {
     delete ui;
+}
+
+void getAddressToReceive::showRequestPayment()
+{
+    Q_EMIT requestPayment();
+}
+
+void getAddressToReceive::setModel(WalletModel *model)
+{
+    this->model = model;
+}
+
+void getAddressToReceive::copyToClipboard()
+{
+    GUIUtil::setClipboard(address);
+}
+
+void getAddressToReceive::getNewAddress()
+{
+    address = model->getAddressTableModel()->addRow(AddressTableModel::Receive, "", "");
+
+#ifdef USE_QRCODE
+    QString uri = "navcoin:" + address;
+    ui->lblQRCode->setText("");
+    if(!uri.isEmpty())
+    {
+        // limit URI length
+        if (uri.length() > MAX_URI_LENGTH)
+        {
+            ui->lblQRCode->setText(tr("Resulting URI too long, try to reduce the text for label / message."));
+        } else {
+            QRcode *code = QRcode_encodeString(uri.toUtf8().constData(), 0, QR_ECLEVEL_L, QR_MODE_8, 1);
+            if (!code)
+            {
+                ui->lblQRCode->setText(tr("Error encoding URI into QR Code."));
+                return;
+            }
+            QImage qrImage = QImage(code->width + 8, code->width + 8, QImage::Format_RGB32);
+            qrImage.fill(0xffffff);
+            unsigned char *p = code->data;
+            for (int y = 0; y < code->width; y++)
+            {
+                for (int x = 0; x < code->width; x++)
+                {
+                    qrImage.setPixel(x + 4, y + 4, ((*p & 1) ? 0x0 : 0xffffff));
+                    p++;
+                }
+            }
+            QRcode_free(code);
+
+            QImage qrAddrImage = QImage(QR_IMAGE_SIZE, QR_IMAGE_SIZE+20, QImage::Format_RGB32);
+            qrAddrImage.fill(0xffffff);
+            QPainter painter(&qrAddrImage);
+            painter.drawImage(0, 0, qrImage.scaled(QR_IMAGE_SIZE, QR_IMAGE_SIZE));
+            QFont font = GUIUtil::fixedPitchFont();
+            font.setPixelSize(12);
+            painter.setFont(font);
+            QRect paddedRect = qrAddrImage.rect();
+            paddedRect.setHeight(QR_IMAGE_SIZE+12);
+            painter.drawText(paddedRect, Qt::AlignBottom|Qt::AlignCenter, address);
+            painter.end();
+
+            ui->lblQRCode->setPixmap(QPixmap::fromImage(qrAddrImage));
+        }
+    }
+#endif
 }
--- src/qt/getaddresstoreceive.h
+++ src/qt/getaddresstoreceive.h
@@ -1,6 +1,7 @@
 #ifndef GETADDRESSTORECEIVE_H
 #define GETADDRESSTORECEIVE_H
 
+#include "walletmodel.h"
 #include <QWidget>
 
 namespace Ui {
@@ -14,9 +15,22 @@ class getAddressToReceive : public QWidget
 public:
     explicit getAddressToReceive(QWidget *parent = 0);
     ~getAddressToReceive();
+    void setModel(WalletModel *model);
+
+public Q_SLOTS:
+    void getNewAddress();
 
 private:
     Ui::getAddressToReceive *ui;
+    WalletModel *model;
+    QString address;
+
+Q_SIGNALS:
+    void requestPayment();
+
+private Q_SLOTS:
+    void showRequestPayment();
+    void copyToClipboard();
 };
 
 #endif // GETADDRESSTORECEIVE_H
