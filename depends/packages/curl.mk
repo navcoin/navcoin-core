ffbc488b61ebdbd27d443f3ba5fb602062fc271c
alex v <alexvazquezmusic@me.com>
2017-03-12 00:48:50 +0100

    Create curl.mk

---
+++ depends/packages/curl.mk
@@ -0,0 +1,20 @@
+package=curl
+$(package)_version=7.53.1
+$(package)_download_path=https://curl.haxx.se/download/
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=64f9b7ec82372edb8eaeded0a9cfa62334d8f98abc65487da01188259392911d
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+endef
