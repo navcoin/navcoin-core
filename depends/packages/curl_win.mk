dd2aee430ebe8c49ce04d23cf41b7b0276015518
alex v <alexvazquezmusic@me.com>
2017-03-16 14:50:55 +0100

    Update curl_win.mk

--- depends/packages/curl_win.mk
+++ depends/packages/curl_win.mk
@@ -7,8 +7,8 @@ $(package)_dependencies=openssl
 
 define $(package)_set_vars
   $(package)_config_opts=--with-ssl=$(host_prefix) --with-random=/dev/urandom
-  $(package)_config_opts_x86_64_mingw32=mingw64
-  $(package)_config_opts_i686_mingw32=mingw32
+  $(package)_config_opts_x86_64_mingw32=mingw64 --enable-static
+  $(package)_config_opts_i686_mingw32=mingw32 --enable-static
 endef
 
 define $(package)_config_cmds
