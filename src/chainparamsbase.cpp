aaace810ca08aee93eaa00cb41eaca2057538dc6
alex v <alex@alohechotecho.com>
2017-04-19 13:16:21 +0200

    testnet: sets port for rpc server

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 44444;
+        nRPCPort = 44445;
         strDataDir = "testnet3";
     }
 };
@@ -59,7 +59,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 44444;
+        nRPCPort = 44446;
         strDataDir = "regtest";
     }
 };
