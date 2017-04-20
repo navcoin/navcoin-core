14ba5ef285fcb579d4c5a906d849212d993ea580
alex v <alex@alohechotecho.com>
2017-04-20 19:45:04 +0200

    staking: removes old block version

--- src/miner.cpp
+++ src/miner.cpp
@@ -200,7 +200,6 @@ CBlockTemplate* BlockAssembler::CreateNewBlock(const CScript& scriptPubKeyIn, bo
         UpdateTime(pblock, chainparams.GetConsensus(), pindexPrev);
     pblock->nBits          = fProofOfStake ? GetNextTargetRequired(pindexPrev, true) : GetNextWorkRequired(pindexPrev, pblock, chainparams.GetConsensus());
     pblock->nNonce         = 0;
-    pblock->nVersion       = VERSIONBITS_LAST_OLD_BLOCK_VERSION;
     pblocktemplate->vTxSigOpsCost[0] = WITNESS_SCALE_FACTOR * GetLegacySigOpCount(pblock->vtx[0]);
 
     if (pFees)
