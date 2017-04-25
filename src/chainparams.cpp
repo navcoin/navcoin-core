d52892ed769a3953d22443a198e5cb50b71d1a14
alex v <alex@alohechotecho.com>
2017-04-25 18:37:50 +0200

    voting: sets final date

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -99,7 +99,7 @@ public:
 
         // Deployment of SegWit (BIP141 and BIP143)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 5;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1492875152; //1493596800; // May 1st, 2017
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1493596800; // May 1st, 2017
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1525132800; // May 1st, 2018
 
         /**
