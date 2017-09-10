9b7a6745b0f7a3f6f4f2de2afba8e78f21f8c128
alex v <alex@alohechotecho.com>
2017-09-10 12:39:40 +0200

    gui: update price action

--- src/qt/navcoingui.h
+++ src/qt/navcoingui.h
@@ -145,6 +145,7 @@ private:
     const PlatformStyle *platformStyle;
 
     QAction *toggleStakingAction;
+    QAction *updatePriceAction;
 
     /** Create the main UI actions. */
     void createActions();
