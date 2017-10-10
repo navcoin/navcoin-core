aff2fb2b398cd5227a143420d32da79ed35fdb8b
alex v <alex@alohechotecho.com>
2017-10-10 23:37:31 +0200

    gui: updates voting dialog text

--- src/qt/navcoingui.cpp
+++ src/qt/navcoingui.cpp
@@ -1128,7 +1128,7 @@ void NavCoinGUI::showVotingDialog()
 
     QMessageBox msgBox;
     msgBox.setText(tr("Important network notice."));
-    msgBox.setInformativeText(tr("The Nav Coin Network is currently voting on activating Segregated Witness. As a participant in our network, we value your input and the decision ultimately is yours. Please cast your vote. <br><br>For more information on Segregated Witness, please visit <a href=\"https://bitcoincore.org/en/2016/01/26/segwit-benefits/\">this link</a><br><br>Would you like the Nav Coin Network to activate Segregated Witness?"));
+    msgBox.setInformativeText(tr("The Nav Coin Network is currently voting on introducing changes on the consensus protocol. As a participant in our network, we value your input and the decision ultimately is yours. Please cast your vote. <br><br>For more information on the proposal, please visit <a href=\"https://navcoin.org/community-fund\">this link</a><br><br>Would you like the Nav Coin Network to create a decentralized community fund using part of the staking rewards?"));
     QAbstractButton *myYesButton = msgBox.addButton(tr("Yes"), QMessageBox::YesRole);
     msgBox.addButton(trUtf8("No"), QMessageBox::NoRole);
     msgBox.setIcon(QMessageBox::Question);
