cb91d0eccb602633157a17811946e5235ede69c2
alex v <alex@alohechotecho.com>
2017-10-26 01:09:44 +0200

    fix: add missing cases

--- src/script/ismine.cpp
+++ src/script/ismine.cpp
@@ -50,6 +50,7 @@ isminetype IsMine(const CKeyStore &keystore, const CScript& scriptPubKey)
     {
     case TX_NONSTANDARD:
     case TX_NULL_DATA:
+    case TX_CONTRIBUTION:
         break;
     case TX_PUBKEY:
         keyID = CPubKey(vSolutions[0]).GetID();
