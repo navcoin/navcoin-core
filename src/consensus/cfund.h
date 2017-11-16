6f4636923aaac7f376d6a6cdfe59db43c5334d73
alex v <alex@alohechotecho.com>
2017-11-16 23:21:32 +0100

    cfund: update classes

--- src/consensus/cfund.h
+++ src/consensus/cfund.h
@@ -25,25 +25,47 @@ public:
     unsigned char fState;
     uint256 hash;
     uint256 paymentHash;
-
+    int votes;
 
     CPaymentRequest() { SetNull(); }
 
     void SetNull() {
         nAmount = 0;
         fState = 0;
+        votes = 0;
         hash = uint256();
         paymentHash = uint256();
     }
 
     bool IsNull() {
-        return (nAmount == 0 && fState == 0);
+        return (nAmount == 0 && fState == 0 && votes == 0);
+    }
+
+    void Accept() {
+        fState = ACCEPTED;
+    }
+
+    void Reject() {
+        fState = REJECTED;
     }
 
     std::string ToString() const
     {
-        return strprintf("CPaymentRequest(amount=%u, fState=%u, hash=%s, paymentHash=%s)",
-                         nAmount, fState, hash.ToString().substr(0,10), paymentHash.ToString().substr(0,10));
+        std::string sFlags;
+        if(IsAccepted())
+            sFlags = "accepted";
+        if(IsRejected())
+            sFlags = "rejected";
+        return strprintf("CPaymentRequest(amount=%u, fState=%s, votes=%u, hash=%s, paymentHash=%s)",
+                         nAmount, sFlags, votes, hash.ToString().substr(0,10), paymentHash.ToString().substr(0,10));
+    }
+
+    bool IsAccepted() const {
+        return fState == ACCEPTED;
+    }
+
+    bool IsRejected() const {
+        return fState == REJECTED;
     }
 
     ADD_SERIALIZE_METHODS;
@@ -52,9 +74,15 @@ public:
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
         READWRITE(nAmount);
         READWRITE(fState);
+        READWRITE(votes);
         READWRITE(hash);
         READWRITE(paymentHash);
     }
+
+private:
+    const char ACCEPTED = 0x01;
+    const char REJECTED = 0x02;
+
 };
 
 class CProposal
@@ -65,11 +93,11 @@ public:
     std::string Address;
     uint32_t nDeadline;
     unsigned char fState;
+    int votes;
     std::vector<CPaymentRequest> vPayments;
     std::string strDZeel;
     uint256 hash;
     uint256 blockhash;
-
 
     CProposal() { SetNull(); }
 
@@ -78,6 +106,7 @@ public:
         nFee = 0;
         Address = "";
         fState = 0;
+        votes = 0;
         nDeadline = 0;
         vPayments.clear();
         strDZeel = "";
@@ -86,17 +115,46 @@ public:
     }
 
     bool IsNull() const {
-        return (nAmount == 0 && nFee == 0 && Address == "" && fState == 0 && nDeadline == 0 && strDZeel == "");
+        return (nAmount == 0 && nFee == 0 && Address == "" && votes == 0 && fState == 0 && nDeadline == 0 && strDZeel == "");
+    }
+
+    void Accept() {
+        fState = ACCEPTED;
+    }
+
+    void Reject() {
+        fState = REJECTED;
     }
 
     std::string ToString() const
     {
+        std::string sFlags;
+        if(IsAccepted())
+            sFlags = "accepted";
+        if(IsRejected())
+            sFlags = "rejected";
         std::string str;
-        str += strprintf("CProposal(hahs=%s, amount=%u, nFee=%u, address=%s, nDeadline=%u, fState=%u, strDZeel=%s, blockhash=%s)",
-                         hash.ToString().substr(0,10), nAmount, nFee, Address, nDeadline, fState, strDZeel, blockhash.ToString().substr(0,10));
+        str += strprintf("CProposal(hash=%s, amount=%u, available=%d, nFee=%u, address=%s, nDeadline=%u, votes=%u, fState=%s, strDZeel=%s, blockhash=%s)",
+                         hash.ToString().substr(0,10), nAmount, GetAvailable(), nFee, Address, nDeadline, votes, sFlags, strDZeel, blockhash.ToString().substr(0,10));
         for (unsigned int i = 0; i < vPayments.size(); i++)
             str += "    " + vPayments[i].ToString() + "\n";
         return str;
+    }
+
+    bool IsAccepted() const {
+        return fState == ACCEPTED;
+    }
+
+    bool IsRejected() const {
+        return fState == REJECTED;
+    }
+
+    CAmount GetAvailable(bool fIncludeRequests = false) const
+    {
+        CAmount initial = nAmount;
+        for (unsigned int i = 0; i < vPayments.size(); i++)
+            if(fIncludeRequests || (!fIncludeRequests && vPayments[i].IsAccepted()))
+                initial -= vPayments[i].nAmount;
     }
 
     ADD_SERIALIZE_METHODS;
@@ -111,11 +169,17 @@ public:
         READWRITE(Address);
         READWRITE(nDeadline);
         READWRITE(fState);
+        READWRITE(votes);
         READWRITE(*const_cast<std::vector<CPaymentRequest>*>(&vPayments));
         READWRITE(strDZeel);
         READWRITE(hash);
         READWRITE(blockhash);
     }
+
+private:
+    const char ACCEPTED = 0x01;
+    const char REJECTED = 0x02;
+
 };
 
 }
