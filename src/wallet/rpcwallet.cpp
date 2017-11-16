7a29766a9ab3ddc95dfde82dd8f38b066851fd25
alex v <alex@alohechotecho.com>
2017-11-16 21:18:52 +0100

    rpc: convert parameter to int

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -524,7 +524,7 @@ UniValue createproposal(const UniValue& params, bool fHelp)
 
     string Address = params[0].get_str();
     CAmount nReqAmount = AmountFromValue(params[1]);
-    int64_t nDeadline = AmountFromValue(params[2]);
+    int64_t nDeadline = params[2].get_int64();
     string sDesc = params.size() == 4 ? params[3].get_str() : "";
 
     wtx.strDZeel = "{\"n\":" + boost::lexical_cast<std::string>(nReqAmount) +",\"a\":\""+ Address +"\",\"d\":"+boost::lexical_cast<std::string>(nDeadline)+",\"s\":\""+ sDesc +"\"}";
