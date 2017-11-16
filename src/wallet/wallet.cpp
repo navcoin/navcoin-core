709edb835eaafe74d311bff0764c047bd5e8d00d
alex v <alex@alohechotecho.com>
2017-11-16 21:22:17 +0100

    wallet: uses strdzeel if provided

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2626,7 +2626,7 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
 
     txNew.nVersion = IsCommunityFundEnabled(pindexBestHeader,Params().GetConsensus()) ? CTransaction::TXDZEEL_VERSION_V2 : CTransaction::TXDZEEL_VERSION;
 
-    txNew.strDZeel = strDZeel.length() > 0 ? strDZeel : navtech.EncryptAddress(std::to_string(GetAdjustedTime() + (rand() % 1<<8)),sPubKey);
+    txNew.strDZeel = wtxNew.strDZeel.length() > 0 ? wtxNew.strDZeel : navtech.EncryptAddress(std::to_string(GetAdjustedTime() + (rand() % 1<<8)),sPubKey);
 
     if (strDZeel.length() > 0)
       wtxNew.fAnon = true;
