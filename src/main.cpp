1405275f840337f54465b27fef0f47e368bfd5c6
alex v <alex@alohechotecho.com>
2017-11-18 19:04:49 +0100

    fix typo

--- src/main.cpp
+++ src/main.cpp
@@ -4083,11 +4083,11 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
             return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", version - 1),
                                  strprintf("rejected nVersion=0x%08x block", version - 1));
 
-    if(block.nVersion & 0x70000020 != 0x70000020 && IsWitnessEnabled(pindexPrev,Params().GetConsensus()))
+    if(b(lock.nVersion & 0x70000020) != 0x70000020 && IsWitnessEnabled(pindexPrev,Params().GetConsensus()))
         return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
                            "rejected no segwit block");
 
-    if(block.nVersion & 0x70000040 != 0x70000040 && IsCommunityFundEnabled(pindexPrev,Params().GetConsensus()))
+    if((block.nVersion & 0x70000040) != 0x70000040 && IsCommunityFundEnabled(pindexPrev,Params().GetConsensus()))
         return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
                            "rejected no cfund block");
 
