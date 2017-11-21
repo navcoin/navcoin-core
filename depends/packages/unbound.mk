a6a8d782b4fa093b9a3413c86568560d44124012
alex v <alexvazquezmusic@me.com>
2017-11-21 12:34:52 +0100

    Update unbound.mk

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -3,11 +3,13 @@ $(package)_version=1.6.7
 $(package)_download_path=http://unbound.net/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=4e7bd43d827004c6d51bef73adf941798e4588bdb40de5e79d89034d69751c9f
+$(package)_dependencies=openssl
 
 define $(package)_preprocess_cmds
 endef
 
 define $(package)_set_vars
+  $(package)_config_opts=--with-ssl=$(host_prefix)/lib
 endef
 
 define $(package)_config_cmds
