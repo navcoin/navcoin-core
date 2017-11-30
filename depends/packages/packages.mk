4c5003185cfa2bb5b13e8915c9d975661dca835f
alex v <alexvazquezmusic@me.com>
2017-11-30 20:06:14 +0100

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -6,10 +6,10 @@ native_packages := native_ccache native_comparisontool
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans libexpat
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
-qt_darwin_packages=qt
+qt_darwin_packages=qt libexpat
 qt_mingw32_packages=qt
 
 wallet_packages=bdb
