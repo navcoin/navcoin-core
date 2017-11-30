e082943b9cc5d7f0741dbd77e58286d485cab21c
alex v <alexvazquezmusic@me.com>
2017-11-30 13:00:22 +0100

    Update unbound.mk

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -5,6 +5,7 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=4e7bd43d827004c6d51bef73adf941798e4588bdb40de5e79d89034d69751c9f
 $(package)_dependencies=openssl
 $(package)_patches=fix_pkg_config.patch
+$(package)_deptrack=1
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/fix_pkg_config.patch
