d9960e1be5dad287a9a0bff9d0eb494bbf7ce11e
alex v <alex@alohechotecho.com>
2017-12-06 14:06:03 +0100

    check for future timestamps when creating tx

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2836,8 +2836,12 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
                 // Note how the sequence number is set to max()-1 so that the
                 // nLockTime set above actually works.
                 BOOST_FOREACH(const PAIRTYPE(const CWalletTx*,unsigned int)& coin, setCoins)
+                {
+                    if(coin.first->nTime > txNew.nTime)
+                        txNew.nTime = coin.first->nTime;
                     txNew.vin.push_back(CTxIn(coin.first->GetHash(),coin.second,CScript(),
                                               std::numeric_limits<unsigned int>::max()-1));
+                }
 
                 // Sign
                 int nIn = 0;
