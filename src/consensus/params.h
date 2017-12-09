63e5b6cf3b81bcf96d242a6dab4b5a522e5ce505
alex v <alex@alohechotecho.com>
2017-12-09 14:45:38 +0100

    sig enabled

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -82,6 +82,7 @@ struct Params {
     int64_t nStakeSplitThreshold;
     int nDailyBlockCount;
     unsigned int nModifierInterval; // time to elapse before new modifier is computed
+    int64_t sigActivationTime;
 
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
 };
