ec9373df920dc0fa6f5bc66008b617f5c05c6ffe
alex v <alex@encrypt-s.com>
2018-02-16 23:49:33 +0100

    net: update hardcoded nodes

--- contrib/seeds/generate-seeds.py
+++ contrib/seeds/generate-seeds.py
@@ -130,7 +130,7 @@ def main():
         process_nodes(g, f, 'pnSeed6_main', 44444)
     g.write('\n')
     with open(os.path.join(indir,'nodes_test.txt'),'r') as f:
-        process_nodes(g, f, 'pnSeed6_test', 44444)
+        process_nodes(g, f, 'pnSeed6_test', 15556)
     g.write('#endif // NAVCOIN_CHAINPARAMSSEEDS_H\n')
             
 if __name__ == '__main__':
