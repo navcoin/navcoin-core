821a31f4a26f3aa6ce6e71bc4cf283126d1633a6
alex v <alex@encrypt-s.com>
2018-02-23 11:33:54 +0100

    Update unbound.mk

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -4,13 +4,6 @@ $(package)_download_path=http://unbound.net/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash= e3b428e33f56a45417107448418865fe08d58e0e7fea199b855515f60884dd49
 $(package)_dependencies=openssl
-$(package)_patches=fix_pkg_config.patch
-$(package)_deptrack=1
-$(package)_cppflags=-I/usr/include
-
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/fix_pkg_config.patch
-endef
 
 define $(package)_set_vars
   $(package)_config_opts=--with-ssl=$(host_prefix) --disable-gost --disable-ecdsa
