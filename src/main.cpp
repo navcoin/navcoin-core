bef989a53bb4089ac243dfba1174e511e68770c6
alex v <alex@alohechotecho.com>
2018-02-28 20:21:24 +0100

    fix: missing parenthesis

--- src/main.cpp
+++ src/main.cpp
@@ -2539,7 +2539,7 @@ public:
 
     bool Condition(const CBlockIndex* pindex, const Consensus::Params& params) const
     {
-        return ((pindex->nVersion & VERSIONBITS_TOP_MASK) == IsSigHFEnabled(Params().GetConsensus(), pindex) ? VERSIONBITS_TOP_BITS_SIG : VERSIONBITS_TOP_BITS) &&
+        return (((pindex->nVersion & VERSIONBITS_TOP_MASK) == IsSigHFEnabled(Params().GetConsensus(), pindex)) ? VERSIONBITS_TOP_BITS_SIG : VERSIONBITS_TOP_BITS) &&
                ((pindex->nVersion >> bit) & 1) != 0 &&
                ((ComputeBlockVersion(pindex->pprev, params) >> bit) & 1) == 0;
     }
@@ -3275,9 +3275,6 @@ void PruneAndFlush() {
 /** Update chainActive and related internal data structures. */
 void static UpdateTip(CBlockIndex *pindexNew, const CChainParams& chainParams) {
     chainActive.SetTip(pindexNew);
-
-    if (!pcfundindex->WriteTipHeight(pindexNew->nHeight))
-        AbortNode("Failed to write tip height to proposal db", "");
 
     // New best block
     nTimeBestReceived = GetTime();
