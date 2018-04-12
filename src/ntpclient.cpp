2c35bd3bdcb317c847f0dc9a63188825566479b3
alex v <alex@alohechotecho.com>
2018-04-12 21:02:19 +0200

    auto adjust clock if user change detected

--- src/ntpclient.cpp
+++ src/ntpclient.cpp
@@ -3,6 +3,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "ntpclient.h"
+#include "random.h"
+#include "timedata.h"
 #include "util.h"
 
 using namespace boost;
@@ -14,7 +16,7 @@ int64_t CNtpClient::getTimestamp()
 
     io_service io_service;
 
-    LogPrint("ntp", "CNtpClient: Opening socket to NTP server %s.\n", sHostName);
+    LogPrint("ntp", "[NTP] Opening socket to NTP server %s.\n", sHostName);
     try
     {
 
@@ -53,7 +55,7 @@ int64_t CNtpClient::getTimestamp()
             if(!FD_ISSET(nativeSocket,&fileDescriptorSet))
             {
 
-                LogPrint("ntp", "CNtpClient: Could not read socket from NTP server %s (Read timeout)\n", sHostName);
+                LogPrint("ntp", "[NTP] Could not read socket from NTP server %s (Read timeout)\n", sHostName);
 
             }
             else
@@ -63,7 +65,7 @@ int64_t CNtpClient::getTimestamp()
                 timeRecv = ntohl((time_t)recvBuf[4]);
                 timeRecv-= 2208988800U;  // Substract 01/01/1970 == 2208988800U
 
-                LogPrint("ntp", "CNtpClient: Received timestamp: %ll \n", (uint64_t)timeRecv);
+                LogPrint("ntp", "[NTP] Received timestamp: %ll \n", (uint64_t)timeRecv);
 
             }
 
@@ -71,7 +73,7 @@ int64_t CNtpClient::getTimestamp()
         catch (std::exception& e)
         {
 
-             LogPrintf("CNtpClient: Could not read clock from NTP server %s (%s)\n", sHostName, e.what());
+             LogPrintf("[NTP] Could not read clock from NTP server %s (%s)\n", sHostName, e.what());
 
         }
 
@@ -79,11 +81,115 @@ int64_t CNtpClient::getTimestamp()
     catch (std::exception& e)
     {
 
-        LogPrintf("CNtpClient: Could not open socket to NTP server %s (%s)\n", sHostName, e.what());
+        LogPrintf("[NTP] Could not open socket to NTP server %s (%s)\n", sHostName, e.what());
 
     }
 
-
-
     return (int64_t)timeRecv;
-  }
+}
+
+bool NtpClockSync() {
+    LogPrintf("[NTP] Starting clock sync...\n");
+
+    std::vector<std::string> vNtpServers;
+    std::vector<int64_t> vResults;
+
+    if (mapMultiArgs["-ntpservers"].size() > 0)
+    {
+        vNtpServers = mapMultiArgs["-ntpservers"];
+    }
+    else
+    {
+        vNtpServers = vDefaultNtpServers;
+    }
+
+    string sReport = "";
+    string sPrevServer = "";
+    int64_t nPrevMeasure = -1;
+
+    random_shuffle(vNtpServers.begin(), vNtpServers.end(), GetRandInt);
+
+    for(unsigned int i = 0; i < vNtpServers.size(); i++)
+    {
+        string s = vNtpServers[i];
+        CNtpClient ntpClient(s);
+        int64_t nTimestamp = ntpClient.getTimestamp();
+        if(nTimestamp > -1)
+        {
+            int64_t nClockDrift = GetTime() - nTimestamp;
+
+            // We push if its the first entry
+            if(vResults.size() == 0)
+            {
+                vResults.push_back(nClockDrift);
+
+                string sign = "";
+                if(nClockDrift > 0)
+                    sign = "+";
+                else if (nClockDrift < 0)
+                    sign = "-";
+
+                sReport += s + "[" + sign + to_string(nClockDrift) + "sec.] ";
+            }
+            // or if we have two cached values, ensuring size of the vector is odd
+            else if (nPrevMeasure != -1)
+            {
+                vResults.push_back(nClockDrift);
+
+                string sign = "";
+                if(nClockDrift > 0)
+                    sign = "+";
+                else if (nClockDrift < 0)
+                    sign = "-";
+
+                sReport += s + "[" + sign + to_string(nClockDrift) + "sec.] ";
+
+                vResults.push_back(nPrevMeasure);
+
+                sign = "";
+                if(nPrevMeasure > 0)
+                    sign = "+";
+                else if (nPrevMeasure < 0)
+                    sign = "-";
+
+                sReport += sPrevServer + "[" + sign + to_string(nPrevMeasure) + "sec.] ";
+
+                nPrevMeasure = -1;
+                sPrevServer = "";
+            }
+            else
+            {
+                nPrevMeasure = nClockDrift;
+                sPrevServer = s;
+            }
+
+            if (vResults.size() >= 5)
+                break;
+        }
+    }
+
+    assert(vResults.size() % 2 == 1 || vResults.size() == 0);
+
+    unsigned int nMin = GetArg("-ntpminmeasures", MINIMUM_NTP_MEASURE);
+
+    if (vResults.size() >= nMin)
+    {
+        LogPrintf("[NTP] Measured: %s\n", sReport);
+
+        static CMedianFilter<int64_t> vNtpTimeOffsets(vResults.size(), 0);
+
+        for(unsigned int i = 0; i < vResults.size(); i++)
+        {
+            vNtpTimeOffsets.input(vResults[i]);
+        }
+
+        nNtpTimeOffset = vNtpTimeOffsets.median();
+
+        LogPrintf("[NTP] Calculated offset from median: %d\n", nNtpTimeOffset);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
