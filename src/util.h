5f3c3e12252310b0a6cdd4849f1e3e30f3cc0641
alex v <alex@alohechotecho.com>
2018-04-30 22:05:05 +0200

    add random_string
    adds function to generate a random string of a given length

--- src/util.h
+++ src/util.h
@@ -252,6 +252,22 @@ template <typename Callable> void TraceThread(const char* name,  Callable func)
     }
 }
 
+std::string random_string( size_t length )
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
+}
+
 std::string CopyrightHolders(const std::string& strPrefix);
 
 #endif // NAVCOIN_UTIL_H
