d0538cc828ac43dec11d1823104fbbee009849c6
Paul Sanderson <red010b37@users.noreply.github.com>
2018-05-02 20:11:26 +1200

    Update time stamp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -400,7 +400,7 @@ public:
         // To create a new devnet:
         //
         // 1) Replace nTimestamp with current timestamp.
-        uint32_t nTimestamp = 1525240247;
+        uint32_t nTimestamp = 1525248575;
         // 2) Rebuild
         // 3) Launch daemon. It'll calculate the new parameters.
         // 4) Update the following variables with the new values:
