523d8b557a982aa5f54038e24465fcae03be66bf
alex v <alex@encrypt-s.com>
2018-07-19 23:08:48 +0200

    remove duplicate case

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1031,12 +1031,6 @@ bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, un
                 }
                 break;
 
-                case OP_COINSTAKE:
-                {
-                    stack.push_back(checker.IsCoinStake() ? vchTrue : vchFalse);
-                }
-                break;
-
                 default:
                     return set_error(serror, SCRIPT_ERR_BAD_OPCODE);
             }
