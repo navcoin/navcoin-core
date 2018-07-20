9775c41c4a788feabaa249eea8105e368515ff0a
alex v <alex@encrypt-s.com>
2018-07-20 16:51:10 +0200

    expat

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -3,7 +3,7 @@ $(package)_version=1.7.3
 $(package)_download_path=http://unbound.net/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=c11de115d928a6b48b2165e0214402a7a7da313cd479203a7ce7a8b62cba602d
-$(package)_dependencies=openssl
+$(package)_dependencies=openssl expat
 
 define $(package)_set_vars
   $(package)_config_opts=--with-ssl=$(host_prefix) --disable-gost --disable-ecdsa
