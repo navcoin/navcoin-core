d41462b8508020b57d88886936bbf2710e8f59a0
alex v <alex@encrypt-s.com>
2018-07-20 20:34:49 +0200

    move open alias to exclusively use unbound (#250)
    * require unbound
    
    * add unbounb package
    
    * add curl depend
    
    * rem expat depend
    
    * unbound version
    
    * exclusively unbound
    
    * exclusively unbound
    
    * expat
    
    * add unbound libs
    
    * add unbound libs for qt
    
    * platform dependent
    
    * add unbound libs for qttest
    
    * fix
    
    * fix
    
    * linux dependencies
    
    * add expat to every os
    
    * last changes

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,10 +1,10 @@
-packages:=boost openssl libevent curl zeromq
+packages:=boost openssl libevent curl zeromq unbound expat
 native_packages := native_ccache native_comparisontool
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
