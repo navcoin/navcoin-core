f3fd7b7821e732d870874aa86f6003e99e9f3231
alex v <alex@encrypt-s.com>
2018-07-22 11:53:25 +0200

    don't use getauxval to preserve old glibc compatibility

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -4,6 +4,11 @@ $(package)_download_path=http://unbound.net/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=c11de115d928a6b48b2165e0214402a7a7da313cd479203a7ce7a8b62cba602d
 $(package)_dependencies=openssl expat
+$(package)_patches=getauxval.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/getauxval.patch
+endef
 
 define $(package)_set_vars
   $(package)_config_opts=--with-ssl=$(host_prefix) --disable-gost --disable-ecdsa --disable-shared --with-pic
