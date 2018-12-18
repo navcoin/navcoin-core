845834f5903c9276575a5d2e4d133bb3d8c83634
djksa <44987944+djksa@users.noreply.github.com>
2018-12-19 10:42:22 +1300

    Update qa/rpc-tests/rawtransactions.py
    Co-Authored-By: dy5es41 <dy5es41@protonmail.com>

--- qa/rpc-tests/rawtransactions.py
+++ qa/rpc-tests/rawtransactions.py
@@ -143,7 +143,6 @@ class RawTransactionsTest(NavCoinTestFramework):
         #self.nodes[0].generate(1)
         self.sync_all()
         assert_equal(self.nodes[0].getbalance(), bal+Decimal('50.00000000')+Decimal('2.19000000')) #block reward + tx
-
         inputs  = [ {'txid' : "1d1d4e24ed99057e84c3f80fd8fbec79ed9e1acee37da269356ecea000000000", 'vout' : 1, 'sequence' : 1000}]
         outputs = { self.nodes[0].getnewaddress() : 1 }
         rawtx   = self.nodes[0].createrawtransaction(inputs, outputs)
