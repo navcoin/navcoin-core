e5314b63a6afd37184cec31e3103530aa7a92e63
alex v <alex@encrypt-s.com>
2018-12-29 03:45:55 +0100

    adds missing check

--- src/main.cpp
+++ src/main.cpp
@@ -3298,6 +3298,8 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                     return state.DoS(100, error("CheckBlock() : payment request not mature enough."));
                 if(block.vtx[0].vout[i].nValue != prequest.nAmount || prequest.fState != CFund::ACCEPTED || proposal.Address != CNavCoinAddress(address).ToString())
                     return state.DoS(100, error("CheckBlock() : coinbase output does not match an accepted payment request"));
+                else if(prequest.paymenthash != uint256())
+                    return state.DoS(100, error("CheckBlock() : coinbase output tries to pay an already paid payment request"));
                 else {
                     std::vector<std::pair<uint256, CFund::CPaymentRequest> > paymentRequestIndex;
                     prequest.paymenthash = block.GetHash();
