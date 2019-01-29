5cb057ec98a173f08ffcdbe722be4fc19ff9a0a6
djksa <nopumper@gmail.com>
2019-01-29 16:42:03 +1300

    fixed error

--- qa/rpc-tests/receivedby.py
+++ qa/rpc-tests/receivedby.py
@@ -30,7 +30,7 @@ class ReceivedByTest(NavCoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 4
-        self.setup_clean_chain = False
+        self.setup_clean_chain = True
 
     def setup_nodes(self):
         return start_nodes(self.num_nodes, self.options.tmpdir)
