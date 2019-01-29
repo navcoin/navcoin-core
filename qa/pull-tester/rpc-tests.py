2217ae846e57a44bfec912d179f3f2317fa59ffe
Hayden <hdnsimpson@gmail.com>
2019-01-29 13:22:33 +1300

    multi_rpc already works

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -119,7 +119,7 @@ testScripts = [
 #    'mempool_reorg.py',
 #    'mempool_limit.py',
 #    'httpbasics.py',
-#    'multi_rpc.py',
+     'multi_rpc.py',
      'zapwallettxes.py',
 #    'proxy_test.py',
 #    'merkle_blocks.py',
