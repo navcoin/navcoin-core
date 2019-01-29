6dc2a67f382a862804bc61327cf682a14413d54a
Hayden <hdnsimpson@gmail.com>
2019-01-29 13:32:59 +1300

    added sendtoaddress test

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -167,6 +167,7 @@ testScripts = [
     'staticr-staking-amount.py',
     'hardfork-451.py',
     'staticr-tx-send.py',
+    'sendtoaddress.py',
 ]
 #if ENABLE_ZMQ:
 #    testScripts.append('zmq_test.py')
