94ff38a19d483cbc45acd655465cc4b260cd329a
dy5es41 <dy5es41@protonmail.com>
2019-02-07 16:08:35 +1300

    added page widgets, fetches cfund proposals and paymetn requests

--- src/qt/communityfundpage.cpp
+++ src/qt/communityfundpage.cpp
@@ -1,6 +1,11 @@
 #include "communityfundpage.h"
 #include "ui_communityfundpage.h"
+#include <QAbstractScrollArea>
 #include "communityfundpage.moc"
+#include <iostream>
+#include "main.h"
+#include "txdb.h"
+#include "consensus/cfund.h"
 
 CommunityFundPage::CommunityFundPage(const PlatformStyle *platformStyle, QWidget *parent) :
     QWidget(parent),
@@ -9,6 +14,53 @@ CommunityFundPage::CommunityFundPage(const PlatformStyle *platformStyle, QWidget
     walletModel(0)
 {
     ui->setupUi(this);
+
+    // Hide scrollArea scroll bar
+    ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    connect(ui->pushButtonProposals, SIGNAL(clicked()), this, SLOT(on_click_pushButtonProposals()));
+    connect(ui->pushButtonPaymentRequests, SIGNAL(clicked()), this, SLOT(on_click_pushButtonPaymentRequests()));
+
+    //fetch cfund info
+    Refresh();
+}
+
+void CommunityFundPage::Refresh()
+{
+    {
+    std::vector<CFund::CProposal> vec;
+    if(pblocktree->GetProposalIndex(vec))
+    {
+        BOOST_FOREACH(const CFund::CProposal& proposal, vec) {
+            if (proposal.fState != CFund::NIL)
+                continue;
+            std::cout << "got";
+        }
+    }
+    }
+
+    {
+    std::vector<CFund::CPaymentRequest> vec;
+    if(pblocktree->GetPaymentRequestIndex(vec))
+    {
+        BOOST_FOREACH(const CFund::CPaymentRequest& prequest, vec) {
+            if (prequest.fState != CFund::NIL)
+                continue;
+            std::cout << "got";
+        }
+    }
+    }
+}
+
+void CommunityFundPage::on_click_pushButtonProposals()
+{
+    ui->pushButtonPaymentRequests->setEnabled(false);
+}
+
+void CommunityFundPage::on_click_pushButtonPaymentRequests()
+{
+    ui->pushButtonProposals->setEnabled(false);
 }
 
 CommunityFundPage::~CommunityFundPage()
--- src/qt/communityfundpage.h
+++ src/qt/communityfundpage.h
@@ -2,7 +2,6 @@
 #define COMMUNITYFUNDPAGE_H
 
 #include "communityfundpage.moc"
-
 #include <QWidget>
 #include <QPushButton>
 #include <QListView>
@@ -21,16 +20,22 @@ class CommunityFundPage;
 class CommunityFundPage : public QWidget
 {
     //find a fix for Q_OBJECT macro, vtable error
-    //Q_OBJECT
+    Q_OBJECT
 
 public:
     explicit CommunityFundPage(const PlatformStyle *platformStyle, QWidget *parent = 0);
     ~CommunityFundPage();
+    void Refresh();
 
 private:
     Ui::CommunityFundPage *ui;
     ClientModel *clientModel;
     WalletModel *walletModel;
+
+private Q_SLOTS:
+    void on_click_pushButtonProposals();
+    void on_click_pushButtonPaymentRequests();
+
 };
 
 #endif // COMMUNITYFUNDPAGE_H
