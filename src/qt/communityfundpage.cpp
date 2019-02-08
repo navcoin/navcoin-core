018b23358a3a1e9862543311d37bb3dbdb6e5d8e
Hayden <hdnsimpson@gmail.com>
2019-02-08 13:32:33 +1300

    v1 implementation of proposals buttons toggle

--- src/qt/communityfundpage.cpp
+++ src/qt/communityfundpage.cpp
@@ -17,10 +17,13 @@ CommunityFundPage::CommunityFundPage(const PlatformStyle *platformStyle, QWidget
 
     // Hide horizontal scrollArea scroll bar
     ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    //ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     connect(ui->pushButtonProposals, SIGNAL(clicked()), this, SLOT(on_click_pushButtonProposals()));
     connect(ui->pushButtonPaymentRequests, SIGNAL(clicked()), this, SLOT(on_click_pushButtonPaymentRequests()));
+
+    // Enable selection of pushButtonProposals by default
+    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #DBE0E8 }");
+    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #EDF0F3 }");
 
     //fetch cfund info
     Refresh();
@@ -55,12 +58,14 @@ void CommunityFundPage::Refresh()
 
 void CommunityFundPage::on_click_pushButtonProposals()
 {
-    ui->pushButtonPaymentRequests->setEnabled(false);
+    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #DBE0E8 }");
+    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #EDF0F3 }");
 }
 
 void CommunityFundPage::on_click_pushButtonPaymentRequests()
 {
-    ui->pushButtonProposals->setEnabled(false);
+    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #EDF0F3 }");
+    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #DBE0E8 }");
 }
 
 CommunityFundPage::~CommunityFundPage()
