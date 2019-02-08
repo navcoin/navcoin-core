aec57020f19b8784f0c7ecfae3eecf56c1e57f24
Hayden <hdnsimpson@gmail.com>
2019-02-08 15:44:30 +1300

    Pull cfund available and locked funds on refresh

--- src/qt/communityfundpage.cpp
+++ src/qt/communityfundpage.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "main.h"
 #include "txdb.h"
+#include <string>
 
 CommunityFundPage::CommunityFundPage(const PlatformStyle *platformStyle, QWidget *parent) :
     QWidget(parent),
@@ -22,20 +23,29 @@ CommunityFundPage::CommunityFundPage(const PlatformStyle *platformStyle, QWidget
     connect(ui->pushButtonPaymentRequests, SIGNAL(clicked()), this, SLOT(on_click_pushButtonPaymentRequests()));
 
     // Enable selection of pushButtonProposals by default
-    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #DBE0E8 }");
-    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #EDF0F3 }");
+    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #DBE0E8; }");
+    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #EDF0F3; }");
 
     connect(ui->radioButtonAll, SIGNAL(clicked()), this, SLOT(on_click_radioButtonAll()));
     connect(ui->radioButtonYourVote, SIGNAL(clicked()), this, SLOT(on_click_radioButtonAll()));
     connect(ui->radioButtonPending, SIGNAL(clicked()), this, SLOT(on_click_radioButtonAll()));
     connect(ui->radioButtonAccepted, SIGNAL(clicked()), this, SLOT(on_click_radioButtonAll()));
     connect(ui->radioButtonRejected, SIGNAL(clicked()), this, SLOT(on_click_radioButtonAll()));
+
     //fetch cfund info
     Refresh();
 }
 
 void CommunityFundPage::Refresh()
 {
+    std::string available = std::to_string(pindexBestHeader->nCFSupply);
+    available.append(" NAV");
+    ui->labelAvailableAmount->setText(QString::fromStdString(available));
+
+    std::string locked = std::to_string(pindexBestHeader->nCFLocked);
+    locked.append(" NAV");
+    ui->labelLockedAmount->setText(QString::fromStdString(locked));
+
     {
     std::vector<CFund::CProposal> vec;
     if(pblocktree->GetProposalIndex(vec))
@@ -63,14 +73,14 @@ void CommunityFundPage::Refresh()
 
 void CommunityFundPage::on_click_pushButtonProposals()
 {
-    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #DBE0E8 }");
-    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #EDF0F3 }");
+    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #DBE0E8; }");
+    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #EDF0F3; }");
 }
 
 void CommunityFundPage::on_click_pushButtonPaymentRequests()
 {
-    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #EDF0F3 }");
-    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #DBE0E8 }");
+    ui->pushButtonProposals->setStyleSheet("QPushButton { background-color: #EDF0F3; }");
+    ui->pushButtonPaymentRequests->setStyleSheet("QPushButton { background-color: #DBE0E8; }");
 }
 
 void CommunityFundPage::on_click_radioButtonAll()
