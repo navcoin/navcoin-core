9944bfc0c33c1d3b492d54bbb035d5aa4715b0f5
Hayden <hdnsimpson@gmail.com>
2019-02-12 14:23:33 +1300

    Filters (but my vote) give correct proposals

--- src/qt/communityfundpage.h
+++ src/qt/communityfundpage.h
@@ -27,7 +27,7 @@ class CommunityFundPage : public QWidget
 public:
     explicit CommunityFundPage(const PlatformStyle *platformStyle, QWidget *parent = 0);
     ~CommunityFundPage();
-    void Refresh();
+    void Refresh(bool all);
 
 private:
     Ui::CommunityFundPage *ui;
