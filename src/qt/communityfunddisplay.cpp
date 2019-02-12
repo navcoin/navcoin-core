8e10b91852db1379654f1e6c878585d8d0cec830
dy5es41 <dy5es41@protonmail.com>
2019-02-12 13:35:14 +1300

    finalised labels in community fund display wideget

--- src/qt/communityfunddisplay.cpp
+++ src/qt/communityfunddisplay.cpp
@@ -13,9 +13,25 @@ CommunityFundDisplay::CommunityFundDisplay(QWidget *parent, CFund::CProposal pro
     //set labels from community fund
     ui->title->setText(QString::fromStdString(proposal.strDZeel));
     ui->labelStatus->setText(QString::fromStdString(proposal.GetState(pindexBestHeader->GetBlockTime())));
-    ui->labelRequested->setNum(int(proposal.nAmount));
-    ui->labelDuration->setNum(int (proposal.nDeadline));
+    ui->labelRequested->setText(QString::fromStdString(std::to_string(proposal.nAmount/100000000.0)));
 
+    //convert seconds to DD/HH/SS for proposal deadline
+    uint64_t deadline = proposal.nDeadline;
+    uint64_t deadline_d = std::floor(deadline/86400);
+    uint64_t deadline_h = std::floor((deadline-deadline_d*86400)/3600);
+    uint64_t deadline_m = std::floor((deadline-(deadline_d*86400 + deadline_h*3600))/60);
+
+    std::string s_deadline = "";
+    if(deadline_d >= 14)
+    {
+        s_deadline = std::to_string(deadline_d) + std::string(" Days");
+    }
+    else
+    {
+        s_deadline = std::to_string(deadline_d) + std::string(" Days ") + std::to_string(deadline_h) + std::string(" Hours ") + std::to_string(deadline_m) + std::string(" Minutes");
+    }
+
+    ui->labelDuration->setText(QString::fromStdString(s_deadline));
 }
 
 CommunityFundDisplay::~CommunityFundDisplay()
