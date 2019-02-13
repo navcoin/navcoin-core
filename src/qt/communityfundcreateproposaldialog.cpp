9b94a16a7f951c6d14efa1564c87301bca014371
dy5es41 <dy5es41@protonmail.com>
2019-02-14 10:48:48 +1300

    added hash of proposal to communityfundviewdetailed.ui, also implemented more of the labels in this ui file

--- src/qt/communityfundcreateproposaldialog.cpp
+++ src/qt/communityfundcreateproposaldialog.cpp
@@ -2,6 +2,13 @@
 #include "ui_communityfundcreateproposaldialog.h"
 #include "guiconstants.h"
 #include "guiutil.h"
+#include "sync.h"
+#include "wallet/wallet.h"
+#include "base58.h"
+#include "main.h"
+#include "wallet/rpcwallet.h"
+#include "wallet/rpcwallet.cpp"
+#include <string>
 
 CommunityFundCreateProposalDialog::CommunityFundCreateProposalDialog(QWidget *parent) :
     QDialog(parent),
@@ -12,12 +19,9 @@ CommunityFundCreateProposalDialog::CommunityFundCreateProposalDialog(QWidget *pa
 
     //maybe set as vecot rof QLineEdit instead?
 
-    ui->spinBoxDays->setMinimum(0);
-    ui->spinBoxDays->setMaximum(999999999);
-    ui->spinBoxHours->setMinimum(0);
-    ui->spinBoxHours->setMaximum(59);
-    ui->spinBoxMinutes->setMinimum(0);
-    ui->spinBoxMinutes->setMaximum(59);
+    ui->spinBoxDays->setRange(0, 999999999);
+    ui->spinBoxHours->setRange(0, 59);
+    ui->spinBoxMinutes->setRange(0, 59);
 
     //connect
     connect(ui->pushButtonClose, SIGNAL(clicked()), this, SLOT(reject()));
@@ -53,7 +57,85 @@ bool CommunityFundCreateProposalDialog::validate()
 
 bool CommunityFundCreateProposalDialog::on_click_pushButtonCreateProposal()
 {
+    if(this->validate())
+    {
+        LOCK2(cs_main, pwalletMain->cs_wallet);
 
+        CNavCoinAddress address("NQFqqMUD55ZV3PJEJZtaKCsQmjLT6JkjvJ"); // Dummy address
+
+        CWalletTx wtx;
+        bool fSubtractFeeFromAmount = false;
+
+        std::string Address = ui->lineEditNavcoinAddress->text().toStdString().c_str();
+
+        CNavCoinAddress destaddress(Address);
+        if (!destaddress.IsValid())
+          return false;
+
+        CAmount nReqAmount = CAmount(100);
+        int64_t nDeadline = ui->spinBoxDays->value()*24*60*60 + ui->spinBoxHours->value()*60*60 + ui->spinBoxMinutes->value()*60;
+
+        if(nDeadline <= 0)
+            return false;
+
+        string sDesc = "owo";
+
+        UniValue strDZeel(UniValue::VOBJ);
+
+        strDZeel.push_back(Pair("n",nReqAmount));
+        strDZeel.push_back(Pair("a",Address));
+        strDZeel.push_back(Pair("d",nDeadline));
+        strDZeel.push_back(Pair("s",sDesc));
+        strDZeel.push_back(Pair("v",IsReducedCFundQuorumEnabled(pindexBestHeader, Params().GetConsensus()) ? CFund::CProposal::CURRENT_VERSION : 2));
+
+        wtx.strDZeel = strDZeel.write();
+        wtx.nCustomVersion = CTransaction::PROPOSAL_VERSION;
+
+        if(wtx.strDZeel.length() > 1024)
+            return false;
+
+        EnsureWalletIsUnlocked();
+
+        //SendMoney(address.Get(), nAmount, fSubtractFeeFromAmount, wtx, "", true);
+        bool donate = true;
+        CAmount curBalance = pwalletMain->GetBalance();
+
+        // Check amount
+        if (nReqAmount <= 0)
+            return false;
+
+        if (nReqAmount > curBalance)
+            return false;
+
+        CScript CFContributionScript;
+
+        // Parse NavCoin address
+        CScript scriptPubKey = GetScriptForDestination(address.Get());
+
+        if(donate)
+          CFund::SetScriptForCommunityFundContribution(scriptPubKey);
+
+        // Create and send the transaction
+        CReserveKey reservekey(pwalletMain);
+        CAmount nFeeRequired;
+        std::string strError;
+        vector<CRecipient> vecSend;
+        int nChangePosRet = -1;
+        CRecipient recipient = {scriptPubKey, nReqAmount, fSubtractFeeFromAmount, ""};
+        vecSend.push_back(recipient);
+        if (!pwalletMain->CreateTransaction(vecSend, wtx, reservekey, nFeeRequired, nChangePosRet, strError, NULL, true, strDZeel.get_str())) {
+            if (!fSubtractFeeFromAmount && nReqAmount + nFeeRequired > pwalletMain->GetBalance());
+        }
+        if (!pwalletMain->CommitTransaction(wtx, reservekey));
+
+        UniValue ret(UniValue::VOBJ);
+
+        ret.push_back(Pair("hash",wtx.GetHash().GetHex()));
+        ret.push_back(Pair("strDZeel",wtx.strDZeel));
+
+        return true;
+    }
+    return true;
 }
 
 CommunityFundCreateProposalDialog::~CommunityFundCreateProposalDialog()
