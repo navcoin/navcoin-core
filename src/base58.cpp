404a2c9da6189776b704f55aed81cc74eab0717d
Pro X <pro@proteanx.com>
2019-02-14 14:16:29 -0500

    Fix copyrights

--- src/base58.cpp
+++ src/base58.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2014-2015 The Bitcoin Core developers
-// Copyright (c) 2018 The DeVault Core developers
+// Copyright (c) 2018 The NavCoin Core developers
+// Copyright (c) 2019 The DeVault Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
