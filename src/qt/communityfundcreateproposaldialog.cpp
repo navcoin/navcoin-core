86be0b359b4137002bbdffc6913045362d88c5ae
dy5es41 <dy5es41@protonmail.com>
2019-02-14 14:11:10 +1300

    hooked up createpaymentrequest fields to button

--- src/qt/communityfundcreateproposaldialog.cpp
+++ src/qt/communityfundcreateproposaldialog.cpp
@@ -17,8 +17,6 @@ CommunityFundCreateProposalDialog::CommunityFundCreateProposalDialog(QWidget *pa
     ui->setupUi(this);
     GUIUtil::setupAddressWidget(ui->lineEditNavcoinAddress, this);
 
-    //maybe set as vecot rof QLineEdit instead?
-
     ui->spinBoxDays->setRange(0, 999999999);
     ui->spinBoxHours->setRange(0, 59);
     ui->spinBoxMinutes->setRange(0, 59);
@@ -62,7 +60,7 @@ bool CommunityFundCreateProposalDialog::on_click_pushButtonCreateProposal()
         LOCK2(cs_main, pwalletMain->cs_wallet);
 
         CNavCoinAddress address("NQFqqMUD55ZV3PJEJZtaKCsQmjLT6JkjvJ"); // Dummy address
-
+        std::cout << "created fake addr\n";
         CWalletTx wtx;
         bool fSubtractFeeFromAmount = false;
 
@@ -72,13 +70,13 @@ bool CommunityFundCreateProposalDialog::on_click_pushButtonCreateProposal()
         if (!destaddress.IsValid())
           return false;
 
-        CAmount nReqAmount = CAmount(100);
+        CAmount nReqAmount = ui->lineEditRequestedAmount->value();
         int64_t nDeadline = ui->spinBoxDays->value()*24*60*60 + ui->spinBoxHours->value()*60*60 + ui->spinBoxMinutes->value()*60;
 
         if(nDeadline <= 0)
             return false;
 
-        string sDesc = "owo";
+        string sDesc = ui->plainTextEditDescription->toPlainText().toStdString();
 
         UniValue strDZeel(UniValue::VOBJ);
 
@@ -96,25 +94,22 @@ bool CommunityFundCreateProposalDialog::on_click_pushButtonCreateProposal()
 
         EnsureWalletIsUnlocked();
 
-        //SendMoney(address.Get(), nAmount, fSubtractFeeFromAmount, wtx, "", true);
         bool donate = true;
         CAmount curBalance = pwalletMain->GetBalance();
 
-        // Check amount
         if (nReqAmount <= 0)
             return false;
 
         if (nReqAmount > curBalance)
             return false;
 
-        CScript CFContributionScript;
-
-        // Parse NavCoin address
+        // Parse NavCoin address (currently crashes wallet)
         CScript scriptPubKey = GetScriptForDestination(address.Get());
 
+        std::cout << "0";
         if(donate)
           CFund::SetScriptForCommunityFundContribution(scriptPubKey);
-
+       std::cout << "1";
         // Create and send the transaction
         CReserveKey reservekey(pwalletMain);
         CAmount nFeeRequired;
@@ -123,16 +118,21 @@ bool CommunityFundCreateProposalDialog::on_click_pushButtonCreateProposal()
         int nChangePosRet = -1;
         CRecipient recipient = {scriptPubKey, nReqAmount, fSubtractFeeFromAmount, ""};
         vecSend.push_back(recipient);
+        std::cout << "2";
         if (!pwalletMain->CreateTransaction(vecSend, wtx, reservekey, nFeeRequired, nChangePosRet, strError, NULL, true, strDZeel.get_str())) {
+            std::cout << "3";
             if (!fSubtractFeeFromAmount && nReqAmount + nFeeRequired > pwalletMain->GetBalance());
+            std::cout << "4";
         }
         if (!pwalletMain->CommitTransaction(wtx, reservekey));
+        std::cout << "5";
 
         UniValue ret(UniValue::VOBJ);
 
         ret.push_back(Pair("hash",wtx.GetHash().GetHex()));
         ret.push_back(Pair("strDZeel",wtx.strDZeel));
 
+        std::cout << "all valid\n";
         return true;
     }
     return true;
