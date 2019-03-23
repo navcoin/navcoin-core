8020dff470022dd2bd5d53d952c657915af2579a
v xela <alex@encrypt-s.com>
2019-03-23 17:04:26 +0100

    benchmarking util

---
+++ src/utils/cbenchutil.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2019 The NavCoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "utiltime.h"
+
+#ifndef CBENCHUTIL_H
+#define CBENCHUTIL_H
+
+
+class CBenchUtil
+{
+public:
+    CBenchUtil()
+    {
+        vCheckpoints.push_back(GetTimeMicros());
+    }
+    int64_t Checkpoint()
+    {
+        vCheckpoints.push_back(GetTimeMicros());
+        assert(vCheckpoints.size() >= 2);
+        return vCheckpoints[vCheckpoints.size()-1] - vCheckpoints[vCheckpoints.size()-2];
+    }
+private:
+    std::vector<int64_t> vCheckpoints;
+};
+
+#endif // CBENCHUTIL_H
