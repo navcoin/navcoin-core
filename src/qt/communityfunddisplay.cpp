1bb908af4f542581822ba5e212ef3216312f5442
alex v <alex@encrypt-s.com>
2019-04-13 23:29:53 +0200

    fix

--- src/qt/communityfunddisplay.cpp
+++ src/qt/communityfunddisplay.cpp
@@ -94,7 +94,7 @@ void CommunityFundDisplay::refresh()
         std::time_t t = static_cast<time_t>(proptime);
         std::stringstream ss;
 	char buf[24];
-	if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t))
+	if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t)))
 	    ss << buf;
         ui->labelTitleDuration->setText(QString::fromStdString(expiry_title));
         ui->labelDuration->setText(QString::fromStdString(ss.str().erase(10, 9)));
@@ -108,7 +108,8 @@ void CommunityFundDisplay::refresh()
             std::string expiry_title = "Expired on: ";
             std::time_t t = static_cast<time_t>(proptime);
             std::stringstream ss;
-            if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t))
+	    char buf[24];
+            if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t)))
 		ss << buf;
             ui->labelTitleDuration->setText(QString::fromStdString(expiry_title));
             ui->labelDuration->setText(QString::fromStdString(ss.str().erase(10, 9)));
