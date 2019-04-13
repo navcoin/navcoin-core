6b7e4a99dc2366ba30e374a9dadd33d9b7810aa3
alex v <alex@encrypt-s.com>
2019-04-13 21:44:06 +0200

    move from put_time to strftime for gcc v4.8 compatibility

--- src/qt/communityfunddisplaypaymentrequest.cpp
+++ src/qt/communityfunddisplaypaymentrequest.cpp
@@ -81,7 +81,8 @@ void CommunityFundDisplayPaymentRequest::refresh()
         std::string duration_title = "Accepted on: ";
         std::time_t t = static_cast<time_t>(proptime);
         std::stringstream ss;
-        ss << std::put_time(std::gmtime(&t), "%c %Z");
+        if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t))
+            ss << buf;
         ui->labelTitleDuration->setText(QString::fromStdString(duration_title));
         ui->labelDuration->setText(QString::fromStdString(ss.str().erase(10, 9)));
     }
@@ -101,7 +102,8 @@ void CommunityFundDisplayPaymentRequest::refresh()
         std::string expiry_title = "Rejected on: ";
         std::time_t t = static_cast<time_t>(proptime);
         std::stringstream ss;
-        ss << std::put_time(std::gmtime(&t), "%c %Z");
+        if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t))
+            ss << buf;
         ui->labelTitleDuration->setText(QString::fromStdString(expiry_title));
         ui->labelDuration->setText(QString::fromStdString(ss.str().erase(10, 9)));
     }
@@ -114,7 +116,8 @@ void CommunityFundDisplayPaymentRequest::refresh()
             std::string expiry_title = "Expired on: ";
             std::time_t t = static_cast<time_t>(proptime);
             std::stringstream ss;
-            ss << std::put_time(std::gmtime(&t), "%c %Z");
+            if (strftime(buf, sizeof(buf), "%c %Z", std::gmtime(&t))
+                ss << buf;
             ui->labelTitleDuration->setText(QString::fromStdString(expiry_title));
             ui->labelDuration->setText(QString::fromStdString(ss.str().erase(10, 9)));
         }
