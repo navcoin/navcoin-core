c4f7dc6d0d8fc0fbdc0095a0e269fb7280515a07
Barry Deeney <mxaddict@codedmaster.com>
2019-05-04 12:23:03 +0800

    Updated libexpat and unbound versions then forced unbound to compile with /depends libexpat

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -1,8 +1,8 @@
 package=unbound
-$(package)_version=1.7.3
+$(package)_version=1.9.0
 $(package)_download_path=http://unbound.net/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=c11de115d928a6b48b2165e0214402a7a7da313cd479203a7ce7a8b62cba602d
+$(package)_sha256_hash=415af94b8392bc6b2c52e44ac8f17935cc6ddf2cc81edfb47c5be4ad205ab917
 $(package)_dependencies=openssl expat
 $(package)_patches=getentropy.patch
 
@@ -11,7 +11,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=--with-ssl=$(host_prefix) --disable-gost --disable-ecdsa --disable-shared --with-pic
+  $(package)_config_opts=--with-ssl=$(host_prefix) --with-libexpat=$(host_prefix) --disable-gost --disable-ecdsa --disable-shared --with-pic
 endef
 
 define $(package)_config_cmds
