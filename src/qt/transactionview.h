98db5464ec269ac3f6ff92ec671bbf01855904b1
Barry Deeney <mxaddict@codedmaster.com>
2019-07-28 11:50:18 +0800

    Resized the status column to fit the icon size better

--- src/qt/transactionview.h
+++ src/qt/transactionview.h
@@ -50,7 +50,7 @@ public:
     };
 
     enum ColumnWidths {
-        STATUS_COLUMN_WIDTH = 30,
+        STATUS_COLUMN_WIDTH = 23,
         WATCHONLY_COLUMN_WIDTH = 23,
         DATE_COLUMN_WIDTH = 120,
         TYPE_COLUMN_WIDTH = 113,
