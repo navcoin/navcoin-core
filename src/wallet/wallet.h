ae43cda9818a3f086996b89a71c7a4f7172fd2c2
Dan Tudor <dtudor01@gmail.com>
2019-08-09 13:18:51 +0100

    Sort available coins to show preference for age (#17)
    * Sort available coins to show preference for age
    
    * We want oldest first

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -514,6 +514,14 @@ public:
 };
 
 
+struct sortByCoinAgeDescending
+{
+    inline bool operator() (const COutput& cOutput1, const COutput& cOutput2)
+    {
+        return (cOutput1.tx->nTime < cOutput2.tx->nTime);
+    }
+};
+
 
 
 /** Private key that includes an expiration date in case it never gets used. */
