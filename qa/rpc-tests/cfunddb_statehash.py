cf25bff59036f1e2e2bd16233793f09eca178478
alex v <alex@encrypt-s.com>
2019-11-13 00:20:15 +0100

    extend qa/rpc-tests/cfunddb_statehash.py

--- qa/rpc-tests/cfunddb_statehash.py
+++ qa/rpc-tests/cfunddb_statehash.py
@@ -16,22 +16,59 @@ class CFundDBStateHash(NavCoinTestFramework):
 
     def setup_network(self):
         self.nodes = []
-        self.nodes.append(start_node(0, self.options.tmpdir, ["-fakecfunddbstatehash=93020ecd66b76b08a3e62181151c2bce169003e85c4bdc63170904f1d0669258"]))
-        self.nodes.append(start_node(1, self.options.tmpdir, ["-cfunddbstatehashthreshold=30", "-cfunddbstatehashcount=60"]))
+        self.nodes.append(start_node(0, self.options.tmpdir, ["-fakecfunddbstatehash=93020ecd66b76b08a3e62181151c2bce169003e85c4bdc63170904f1d0669258", "-headerspamfilter=0"]))
+        self.nodes.append(start_node(1, self.options.tmpdir, ["-cfunddbstatehashthreshold=30", "-cfunddbstatehashcount=60", "-headerspamfilter=0"]))
         connect_nodes(self.nodes[0], 1)
+        self.is_network_split = False
 
     def run_test(self):
         self.nodes[1].generate(300)
-        self.nodes[1].createproposal(self.nodes[1].getnewaddress(), 100, 1000, "test")
-        self.nodes[1].generate(10)
+        self.nodes[1].sendtoaddress(self.nodes[0].getnewaddress(),100000)
+        hash=""
+
+        for i in range(20):
+            phash=self.nodes[1].createproposal(self.nodes[1].getnewaddress(), 100, 1000, "test")["hash"]
+            if (hash == ""):
+                hash = phash
+            self.nodes[1].generate(1)
+            sync_blocks(self.nodes)
+            self.nodes[1].proposalvote(phash, 'yes')
+
+        self.nodes[1].invalidateblock(self.nodes[1].getblockhash(311))
+        self.nodes[1].donatefund(100000)
+        self.nodes[1].generate(30)
         sync_blocks(self.nodes)
 
         assert_equal(self.nodes[0].getcfunddbstatehash(), self.nodes[1].getcfunddbstatehash())
 
-        self.nodes[0].generate(30)
+        self.nodes[1].generate(60)
         sync_blocks(self.nodes)
 
         assert_equal(self.nodes[1].getinfo()['errors'],"")
+
+        raw_preq = self.nodes[1].createpaymentrequest(hash, 100, "preq", True)
+
+        # Disconnect Nodes 0 and 1
+        url = urllib.parse.urlparse(self.nodes[1].url)
+        self.nodes[0].disconnectnode(url.hostname+":"+str(p2p_port(1)))
+
+        self.nodes[0].sendrawtransaction(raw_preq)
+        self.nodes[1].sendrawtransaction(raw_preq)
+
+        self.nodes[0].generate(1)
+        self.nodes[1].generate(5)
+
+        assert(self.nodes[0].getbestblockhash() != self.nodes[1].getbestblockhash())
+
+        connect_nodes(self.nodes[0], 1)
+
+        self.sync_all()
+
+        assert_equal(self.nodes[0].getbestblockhash(), self.nodes[1].getbestblockhash())
+
+        self.nodes[0].generate(30)
+        assert_equal(self.nodes[1].getinfo()['errors'],"")
+        assert_equal(self.nodes[0].getcfunddbstatehash(), self.nodes[1].getcfunddbstatehash())
 
         self.nodes[0].generate(1)
         sync_blocks(self.nodes)
