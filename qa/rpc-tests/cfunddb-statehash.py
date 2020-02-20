6aae817d97afce800185233b8c6a22ffe070544f
Barry Deeney <mxaddict@codedmaster.com>
2020-02-20 13:50:59 +0800

    Updated qa/rpc-tests/cfunddb-statehash.py to use connect_nodes_bi

--- qa/rpc-tests/cfunddb-statehash.py
+++ qa/rpc-tests/cfunddb-statehash.py
@@ -61,8 +61,7 @@ class CFundDBStateHash(NavCoinTestFramework):
         assert(self.nodes[0].getbestblockhash() != self.nodes[1].getbestblockhash())
         assert(self.nodes[0].getcfunddbstatehash() != self.nodes[1].getcfunddbstatehash())
 
-
-        connect_nodes(self.nodes[0], 1)
+        connect_nodes_bi(self.nodes, 0, 1)
 
         self.sync_all()
 
