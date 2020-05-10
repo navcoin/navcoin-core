210d03b27ad7e67e2fe856685142ce35be60b85f
0x2830 <0x2830@tuta.io>
2020-05-10 19:19:57 +0200

    Prebuild cmd order autoconf

--- autogen.sh
+++ autogen.sh
@@ -3,11 +3,8 @@
 export LC_ALL=C
 set -e
 git submodule update --init --recursive
+mkdir -p src/bls-signatures/build
 srcdir="$(dirname $0)"
-echo "$srcdir/src/bls-signatures/build"
-mkdir -p "$srcdir/src/bls-signatures/build"
-cd "$srcdir/src/bls-signatures/build"
-cmake ../
 cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
@@ -16,3 +13,7 @@ fi
 which autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
+
+cd "$srcdir/src/bls-signatures/build"
+cmake ../
+cd "$srcdir"
