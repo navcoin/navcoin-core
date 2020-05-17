2fce5aa08ceefdc461767696fb45a9a21c14a35b
alex v <alex@encrypt-s.com>
2020-05-17 02:29:17 +0200

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,9 @@ export LC_ALL=C
 set -e
 git submodule update --init --recursive
 srcdir="$(dirname $0)"
-mkdir -p "$srcdir/src/bls-signatures/build"
+mkdir -p src/bls-signatures/build
+cmake -Bsrc/bls-signatures/build -Hsrc/bls-signatures
+echo "distdir: all" >> src/bls-signatures/build/Makefile
 cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
@@ -13,8 +15,3 @@ fi
 which autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
-
-cd "$srcdir/src/bls-signatures/build"
-cmake ../
-echo "distdir: all" >> Makefile
-cd "$srcdir"
