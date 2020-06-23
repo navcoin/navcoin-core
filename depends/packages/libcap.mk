3723c0edacd63f4c58374922e6ff4d94b4af62ff
Barry Deeney <mxaddict@codedmaster.com>
2020-06-24 07:54:12 +0800

    Force libcap to use static lib

--- depends/packages/libcap.mk
+++ depends/packages/libcap.mk
@@ -5,6 +5,6 @@ $(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_sha256_hash=5048c849bdbbe24d2ca59463142cb279abec5edf3ab6731ab35a596bcf538a49
 
 define $(package)_build_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir)$(host_prefix) prefix=/ lib="lib" install && \
+  $(MAKE) DESTDIR=$($(package)_staging_dir) prefix=$(host_prefix) lib="lib" install && \
       rm $($(package)_staging_dir)$(host_prefix)/lib/libcap.so*
 endef
