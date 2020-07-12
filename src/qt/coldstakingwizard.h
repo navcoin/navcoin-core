b00ec768b715d4463ea182b440b1d4024ff48f9e
alex v <alex@encrypt-s.com>
2020-07-13 00:42:23 +0200

    [GUI] Add voting address to the cold staking wizard (#728)
    * add voting address to cold staking wizard
    
    * Update coldstakingwizard.cpp

--- src/qt/coldstakingwizard.h
+++ src/qt/coldstakingwizard.h
@@ -53,10 +53,12 @@ public:
 private:
     QLabel *spendingAddressLabel;
     QLabel *stakingAddressLabel;
+    QLabel *votingAddressLabel;
     QLabel *descriptionLabel;
     QLabel *errorLabel;
     QLineEdit *spendingAddressLineEdit;
     QLineEdit *stakingAddressLineEdit;
+    QLineEdit *votingAddressLineEdit;
 };
 
 class ColdStakingAddressPage : public QWizardPage
