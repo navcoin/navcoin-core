9495502aa53ab52a65ec505910eb2c8d45411d8f
alex v <alex@encrypt-s.com>
2020-08-07 16:22:21 +0200

    Update getaddresstoreceive.cpp (#733)

--- src/qt/getaddresstoreceive.cpp
+++ src/qt/getaddresstoreceive.cpp
@@ -77,7 +77,7 @@ void getAddressToReceive::getNewAddress()
 
 void getAddressToReceive::getColdStakingAddress()
 {
-    if (!IsCommunityFundEnabled(chainActive.Tip(),Params().GetConsensus()))
+    if (!IsColdStakingEnabled(chainActive.Tip(),Params().GetConsensus()))
         QMessageBox::warning(this, tr("Action not available"),
                              "<qt>Cold Staking is not active yet.</qt>");
     else {
