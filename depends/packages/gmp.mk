0b48c03de2ffb0afde94526cb421ea42fa74493f
0x2830 <0x2830@tuta.io>
2020-08-29 20:41:13 +0200

    Update gmp.mk

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -5,17 +5,19 @@ $(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_sha256_hash=258e6cd51b3fbdfc185c716d55f82c08aff57df0c6fbd143cf6ed561267a1526
 
 define $(package)_set_vars
-  $(package)_config_opts=--enable-cxx --disable-shared
+  $(package)_config_opts=--disable-shared --with-pic
+  $(package)_config_opts_mingw32=--enable-mingw
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf) --host=$(host) --build=$(build)
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
-  $(MAKE) CPPFLAGS='-fPIC'
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
+
