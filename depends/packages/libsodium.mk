4bed2b28b06cc537f592768af2079b0a8f92dced
alex v <alex@encrypt-s.com>
2020-09-02 23:11:06 +0200

    downgrade libsodium to 1.0.17

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,23 +1,18 @@
 package=libsodium
-$(package)_version=1.0.18
+$(package)_version=1.0.17
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
+$(package)_sha256_hash=0cc3dae33e642cc187b5ceb467e0ad0e1b51dcba577de1190e9ffa17766ac2b1
 $(package)_dependencies=
-$(package)_patches=1.0.15-pubkey-validation.diff 1.0.15-signature-validation.diff
-$(package)_config_opts=--enable-static --disable-shared
-$(package)_config_opts_linux=--with-pic
-$(package)_config_opts_darwin=--with-pic
+$(package)_config_opts=
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/1.0.15-pubkey-validation.diff && \
-  patch -p1 < $($(package)_patch_dir)/1.0.15-signature-validation.diff && \
-  cd $($(package)_build_subdir); DO_NOT_UPDATE_CONFIG_SCRIPTS=1 ./autogen.sh && \
+  cd $($(package)_build_subdir); ./autogen.sh && \
   sed -i 's/libsodium 1.0.18/libsodium1018/' configure
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_autoconf) --enable-static --disable-shared
 endef
 
 define $(package)_build_cmds
