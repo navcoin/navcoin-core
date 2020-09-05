e1e0a39fcc01459ecd4d3497b1976f318d783e87
alex v <alex@encrypt-s.com>
2020-09-06 00:07:20 +0200

    update depends

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl libevent zeromq curl gmp expat unbound zlib libsodium
+packages:=boost openssl libevent zeromq curl gmp expat unbound zlib sodium
 
 linux_packages = libseccomp libcap
 
