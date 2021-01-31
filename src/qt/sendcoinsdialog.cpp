65eb7b7856c292a2c962ce39f16b0246f588136b
alex v <alex@encrypt-s.com>
2021-01-31 20:31:06 +0100

    fix

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -276,7 +276,7 @@ void SendCoinsDialog::on_sendButton_clicked()
     const SendCoinsRecipient &rcp = currentTransaction.recipients.first();
     {
         // generate bold amount string
-        QString amount = "<b>" + NavcoinUnits::formatHtmlWithUnit(model->getOptionsModel()->getDisplayUnit(), nTotalAmount, false, NavcoinUnits::SeparatorStyle::separatorStandard, false, rcp.isanon);
+        QString amount = "<b>" + NavcoinUnits::formatHtmlWithUnit(model->getOptionsModel()->getDisplayUnit(), nTotalAmount - (rcp.fSubtractFeeFromAmount ? txFee : 0) , false, NavcoinUnits::SeparatorStyle::separatorStandard, false, rcp.isanon);
         amount.append("</b>");
         // generate monospace address string
         QString splitAddr = rcp.address;
@@ -312,14 +312,7 @@ void SendCoinsDialog::on_sendButton_clicked()
             questionString.append("<hr /><span style='color:#aa0000;'>");
             questionString.append(NavcoinUnits::formatHtmlWithUnit(model->getOptionsModel()->getDisplayUnit(), txFee, false, NavcoinUnits::SeparatorStyle::separatorStandard, false, rcp.isanon));
             questionString.append("</span> ");
-            if (rcp.fSubtractFeeFromAmount)
-            {
-                questionString.append(tr("subtracted as transaction fee"));
-            }
-            else
-            {
-                questionString.append(tr("added as transaction fee"));
-            }
+            questionString.append(tr("added as transaction fee"));
 
             // append transaction size
             questionString.append(" (" + QString::number((double)currentTransaction.getTransactionSize() / 1000) + " kB)");
@@ -328,7 +321,7 @@ void SendCoinsDialog::on_sendButton_clicked()
 
     // add total amount in all subdivision units
     questionString.append("<hr />");
-    CAmount totalAmount = nTotalAmount + (rcp.fSubtractFeeFromAmount ? -txFee : txFee);
+    CAmount totalAmount = nTotalAmount + (rcp.fSubtractFeeFromAmount ? 0 : txFee);
     QStringList alternativeUnits;
 
     // Check if we have selected a display unit that is not NAV
