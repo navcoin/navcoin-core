6b5bd40e23b539ba06a936a6e5b25043c138ea5a
Barry Deeney <mxaddict@codedmaster.com>
2021-10-16 12:22:12 +0800

    Updated OSX SDK version for depends

--- depends/packages/sodium.mk
+++ depends/packages/sodium.mk
@@ -8,7 +8,7 @@ $(package)_patches=disable-glibc-getrandom-getentropy.patch fix-whitespace.patch
 define $(package)_set_vars
 $(package)_config_opts=--enable-static --disable-shared --with-pic="yes"
 $(package)_config_opts+=--prefix=$(host_prefix)
-$(package)_config_opts_darwin=RANLIB="$(host_prefix)/native/bin/x86_64-apple-darwin16-ranlib" AR="$(host_prefix)/native/bin/x86_64-apple-darwin16-ar" CC="$(host_prefix)/native/bin/$($(package)_cc)"
+$(package)_config_opts_darwin=RANLIB="$($(package)_ranlib)" AR="$($(package)_ar)" CC="$($(package)_cc)"
 endef
 
 define $(package)_config_cmds
