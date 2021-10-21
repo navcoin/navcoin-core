a0ea9a53913d6ec2795a682bf3da65ab8a552d7c
Barry Deeney <mxaddict@codedmaster.com>
2021-10-21 19:28:24 +0800

    Updated src/rpc/client.cpp to use std:: directly

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -192,7 +192,7 @@ UniValue ParseNonRFCJSONValue(const std::string& strVal)
 
     if (!jVal.read(std::string("[")+strVal+std::string("]")) ||
         !jVal.isArray() || jVal.size()!=1)
-        throw runtime_error(string("Error parsing JSON:")+strVal);
+        throw std::runtime_error(std::string("Error parsing JSON:")+strVal);
     return jVal[0];
 }
 
