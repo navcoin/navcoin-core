f95b805ba50905430df150ca29cd3eadcf3f151e
Barry Deeney <mxaddict@codedmaster.com>
2021-11-24 06:30:49 +0800

    Updated gitian to use focal instead of bionic (#885)
    * Updated gitian to use focal instead of bionic
    
    * Added missing makefile entry for ctokens/tokenid.h
    
    * Updated security check
    
    * Updated symbols check
    
    * Updated glibc compat to work with 20.04 ubuntu

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -82,6 +82,60 @@ extern "C" float __wrap_log2f(float x)
     return log2f_old(x);
 }
 
+extern "C" double log_old(double x);
+#ifdef __i386__
+__asm(".symver log_old,log@GLIBC_2.0");
+#elif defined(__amd64__)
+__asm(".symver log_old,log@GLIBC_2.2.5");
+#elif defined(__arm__)
+__asm(".symver log_old,log@GLIBC_2.4");
+#elif defined(__aarch64__)
+__asm(".symver log_old,log@GLIBC_2.17");
+#elif defined(__riscv)
+__asm(".symver log_old,log@GLIBC_2.27");
+#endif
+
+extern "C" double __wrap_log(double x)
+{
+    return log_old(x);
+}
+
+extern "C" double exp_old(double x);
+#ifdef __i386__
+__asm(".symver exp_old,exp@GLIBC_2.0");
+#elif defined(__amd64__)
+__asm(".symver exp_old,exp@GLIBC_2.2.5");
+#elif defined(__arm__)
+__asm(".symver exp_old,exp@GLIBC_2.4");
+#elif defined(__aarch64__)
+__asm(".symver exp_old,exp@GLIBC_2.17");
+#elif defined(__riscv)
+__asm(".symver exp_old,exp@GLIBC_2.27");
+#endif
+
+extern "C" double __wrap_exp(double x)
+{
+    return exp_old(x);
+}
+
+extern "C" double pow_old(double x, double y);
+#ifdef __i386__
+__asm(".symver pow_old,pow@GLIBC_2.0");
+#elif defined(__amd64__)
+__asm(".symver pow_old,pow@GLIBC_2.2.5");
+#elif defined(__arm__)
+__asm(".symver pow_old,pow@GLIBC_2.4");
+#elif defined(__aarch64__)
+__asm(".symver pow_old,pow@GLIBC_2.17");
+#elif defined(__riscv)
+__asm(".symver pow_old,pow@GLIBC_2.27");
+#endif
+
+extern "C" double __wrap_pow(double x, double y)
+{
+    return pow_old(x, y);
+}
+
 extern "C" int glob_old(const char * pattern, int flags, int (*errfunc) (const char *epath, int eerrno), glob_t *pglob);
 #ifdef __i386__
 __asm(".symver glob_old,glob@GLIBC_2.0");
@@ -114,11 +168,31 @@ extern "C" int __poll_chk(struct pollfd *fds, nfds_t nfds, int timeout, size_t f
     return poll(fds, nfds, timeout);
 }
 
+extern "C" void explicit_bzero_old(void *dst, size_t len)
+#ifdef __i386__
+__asm(".symver explicit_bzero_old,explicit_bzero@GLIBC_2.0");
+#elif defined(__amd64__)
+__asm(".symver explicit_bzero_old,explicit_bzero@GLIBC_2.2.5");
+#elif defined(__arm__)
+__asm(".symver explicit_bzero_old,explicit_bzero@GLIBC_2.4");
+#elif defined(__aarch64__)
+__asm(".symver explicit_bzero_old,explicit_bzero@GLIBC_2.17");
+#elif defined(__riscv)
+__asm(".symver explicit_bzero_old,explicit_bzero@GLIBC_2.27");
+#endif
+
+extern "C" void __wrap_explicit_bzero (void *dst, size_t len)
+{
+  memset (dst, '\0', len);
+  /* Compiler barrier.  */
+  asm volatile ("" ::: "memory");
+}
+
 extern "C" void __explicit_bzero_chk(void *dst, size_t len, size_t dstlen)
 {
     if (__glibc_unlikely(dstlen < len))
         __chk_fail();
-    explicit_bzero(dst, len);
+    __wrap_explicit_bzero(dst, len);
 }
 
 extern "C" int getentropy(void *buf, size_t len)
